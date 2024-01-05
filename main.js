0b247d0837210b5bef207d8a62d58f14c4534af9
DesertedPotato <dorianjungi@proton.me>
2024-01-05 15:09:42 +0100

    testimonials

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+// Sticky Navbar
+
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle 
     ('window-scroll', window.scrollY > 0)
+})
+
+// Show/Hide FAQ Block
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'bx bx-plus') {
+            icon.className = 'bx bx-minus'
+        } else {
+            icon.className = 'bx bx-plus';
+        }
+    })
 })
